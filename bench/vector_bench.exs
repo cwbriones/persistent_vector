ae7455a9439ca5b1c49fb482ffc87dbf43ffb54b
Christian Briones <cwbriones@gmail.com>
2016-04-19 00:59:21 -0700

    add some benchmarks to keep some perspective

--- bench/vector_bench.exs
+++ bench/vector_bench.exs
@@ -1,33 +1,88 @@
 defmodule VectorBench do
   use Benchfella
 
-  @size 10000
+  @size 1000
   @tuple Tuple.duplicate(0, @size)
   @list Enum.to_list(0..@size)
   @array :array.from_list(@list)
+  @vector PersistentVector.from_list(@list)
 
-  bench "updating an element in a list" do
+  bench "Access: List" do
+    Enum.at(@list, :random.uniform(@size - 1))
+    :ok
+  end
+
+  bench "Access: Tuple" do
+    elem(@tuple, :random.uniform(@size - 1))
+    :ok
+  end
+
+  bench "Access: :array" do
+    :array.get(:random.uniform(@size), @array)
+    :ok
+  end
+
+  bench "Access: PersistentVector" do
+    PersistentVector.get(@vector, :random.uniform(@size))
+    :ok
+  end
+
+  bench "Updates: List" do
     List.replace_at(@list, :random.uniform(@size - 1), 1)
     :ok
   end
 
-  bench "updating an element in a tuple" do
+  bench "Updates: Tuple" do
     put_elem(@tuple, :random.uniform(@size - 1), 1)
     :ok
   end
 
-  bench "update an element in an array" do
+  bench "Updates: Array" do
     :array.set(:random.uniform(@size), 1, @array)
     :ok
   end
 
-  bench "appending an element to a tuple" do
+  bench "Updates: PersistentVector" do
+    PersistentVector.set(@vector, :random.uniform(@size), 1)
+    :ok
+  end
+
+  bench "Append: Tuple" do
     Tuple.append(@tuple, 0)
     :ok
   end
 
-  bench "appending an element to a list" do
+  bench "Append: List" do
     @list ++ [0]
+    :ok
+  end
+
+  bench "Append: PersistentVector" do
+    PersistentVector.append(@vector, 0)
+    :ok
+  end
+
+  bench "Append: Array" do
+    :array.set(:array.size(@array), 1, @array)
+    :ok
+  end
+
+  bench "Last: PersistentVector" do
+    PersistentVector.get(@vector, @vector.size)
+    :ok
+  end
+
+  bench "Last: List" do
+    List.last @list
+    :ok
+  end
+
+  bench "Last: Tuple" do
+    elem(@tuple, tuple_size(@tuple) - 1)
+  end
+
+  bench "Last: Array" do
+    :array.get(:array.size(@array), @array)
     :ok
   end
 end
--- lib/persistent_vector.ex
+++ lib/persistent_vector.ex
@@ -27,58 +27,58 @@ defmodule PersistentVector do
   end
 
   def get(vector, key) do
-    get_priv(vector.root, key, vector.shift)
+    node_get(vector.root, key, vector.shift)
   end
 
-  defp get_priv(node, key, level) when level > 0 do
+  defp node_get(node, key, level) when level > 0 do
     idx = band(key >>> level, @mask)
-    get_priv(elem(node, idx), key, level - @bits)
+    node_get(elem(node, idx), key, level - @bits)
   end
-  defp get_priv(node, key, level), do: elem(node, band(key, @mask))
+  defp node_get(node, key, level), do: elem(node, band(key, @mask))
 
   def append(vector = %__MODULE__{capacity: 0, root: nil}, value) do
-    %__MODULE__{vector | capacity: @width, root: new_node(value), size: 1}
+    %__MODULE__{vector | capacity: @width, root: node_new(value), size: 1}
   end
   def append(vector = %__MODULE__{capacity: cap, root: root, shift: shift, size: cap}, value) do
-    %__MODULE__{vector | capacity: cap <<< @bits, root: new_node(root), shift: shift + @bits} |> append(value)
+    %__MODULE__{vector | capacity: cap <<< @bits, root: node_new(root), shift: shift + @bits} |> append(value)
   end
   def append(vector = %__MODULE__{root: root, shift: shift, size: size}, value) do
-    %__MODULE__{vector | root: set_priv(root, size, value, shift), size: size + 1}
+    %__MODULE__{vector | root: node_set(root, size, value, shift), size: size + 1}
   end
 
   def set(vector = %__MODULE__{root: root, shift: shift}, key, value) do
-    %__MODULE__{vector | root: set_priv(root, key, value, shift)}
+    %__MODULE__{vector | root: node_set(root, key, value, shift)}
   end
 
-  def set_priv(nil, key, value, level) do
+  def node_set(nil, key, value, level) do
     Enum.reduce(0..div(level, @bits), value, fn val, acc ->
-      new_node(acc)
+      node_new(acc)
     end)
   end
-  def set_priv(node, key, value, level) when level > 0 do
+  def node_set(node, key, value, level) when level > 0 do
     idx = band(key >>> level, @mask)
     child = elem(node, idx)
-    put_elem(node, idx, set_priv(child, key, value, level - @bits))
+    put_elem(node, idx, node_set(child, key, value, level - @bits))
   end
-  def set_priv(node, key, value, level) do
+  def node_set(node, key, value, level) do
     idx = band(key, @mask)
     put_elem(node, idx, value)
-  end
-
-  def new_node(value) do
-    Tuple.duplicate(nil, @width) |> put_elem(0, value)
   end
 
   def from_list(list), do: Enum.reduce(list, new, &(append(&2, &1)))
 
   def to_list(vector = %__MODULE__{root: root, shift: shift}) do
-    to_list_priv(root, shift) |> Enum.take(vector.size)
+    node_to_list(root, shift) |> Enum.take(vector.size)
   end
 
-  def to_list_priv(nil, level), do: []
-  def to_list_priv(node, 0), do: Tuple.to_list(node)
-  def to_list_priv(node, level) do
+  def node_to_list(nil, level), do: []
+  def node_to_list(node, 0), do: Tuple.to_list(node)
+  def node_to_list(node, level) do
     Tuple.to_list(node)
-    |> Enum.flat_map(&to_list_priv(&1, level - @bits))
+    |> Enum.flat_map(&node_to_list(&1, level - @bits))
+  end
+
+  def node_new(value) do
+    Tuple.duplicate(nil, @width) |> put_elem(0, value)
   end
 end

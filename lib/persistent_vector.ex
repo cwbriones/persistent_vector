2cb9900c82070faf4ceb3f35c6435ddff4cf2819
Christian Briones <cwbriones@gmail.com>
2016-04-21 01:28:18 -0700

    extract node into its own submodule

--- lib/persistent_vector.ex
+++ lib/persistent_vector.ex
@@ -1,9 +1,66 @@
 defmodule PersistentVector do
   use Bitwise
 
-  @bits 5
+  defmodule Node do
+    @bits 5
+    @width 1 <<< @bits
+    @mask @width - 1
+
+    def bits, do: @bits
+
+    @inline true
+    def new(value \\ nil) do
+      Tuple.duplicate(nil, @width) |> put_elem(0, value)
+    end
+
+    def insert(nil, key, node, level) when level > 0 do
+      idx = band(key >>> level, @mask)
+      tree = Node.new
+      child = insert(elem(tree, idx), key, node, level - @bits)
+      put_elem(tree, idx, child)
+    end
+    def insert(nil, _, node, _), do: node
+    def insert(tree, key, node, level) when level > 0 do
+      idx = band(key >>> level, @mask)
+      child = insert(elem(tree, idx), key, node, level - @bits)
+      put_elem(tree, idx, child)
+    end
+    def insert(tree, key, node, _) do
+      idx = band(key >>> @bits, @mask)
+      put_elem(tree, idx, node)
+    end
+
+    def get(node, key, level) when level > 0 do
+      idx = band(key >>> level, @mask)
+      get(elem(node, idx), key, level - @bits)
+    end
+    def get(node, key, _), do: elem(node, band(key, @mask))
+
+    def set(nil, _, value, level) do
+      Enum.reduce(0..div(level, @bits), value, fn _, acc ->
+        Node.new(acc)
+      end)
+    end
+    def set(node, key, value, level) when level > 0 do
+      idx = band(key >>> level, @mask)
+      child = elem(node, idx)
+      put_elem(node, idx, set(child, key, value, level - @bits))
+    end
+    def set(node, key, value, _) do
+      idx = band(key, @mask)
+      put_elem(node, idx, value)
+    end
+
+    def to_list(nil, _), do: []
+    def to_list(node, 0), do: Tuple.to_list(node)
+    def to_list(node, level) do
+      Tuple.to_list(node)
+      |> Enum.flat_map(&Node.to_list(&1, level - @bits))
+    end
+  end
+
+  @bits Node.bits
   @width 1 <<< @bits
-  @mask @width - 1
 
   defstruct [
     size: 0,
@@ -17,7 +74,7 @@ defmodule PersistentVector do
 
   def new(opts \\ []) do
     default = Keyword.get(opts, :default, 0)
-    tail = Tuple.duplicate(nil, @width)
+    tail = Node.new
 
     %__MODULE__{
       size: 0,
@@ -33,14 +90,8 @@ defmodule PersistentVector do
     elem(tail, key - offset)
   end
   def get(vector, key) do
-    node_get(vector.root, key, vector.shift)
+    Node.get(vector.root, key, vector.shift)
   end
-
-  defp node_get(node, key, level) when level > 0 do
-    idx = band(key >>> level, @mask)
-    node_get(elem(node, idx), key, level - @bits)
-  end
-  defp node_get(node, key, _), do: elem(node, band(key, @mask))
 
   def append(vector = %__MODULE__{tail_offset: offset, size: size}, value) when size - offset < @width do
     tail = put_elem(vector.tail, size - offset, value)
@@ -56,12 +107,12 @@ defmodule PersistentVector do
 
     {new_root, new_shift} = case size - @width do
       ^capacity ->
-        {node_new(root), shift + @bits}
+        {Node.new(root), shift + @bits}
       _ -> {root, shift}
     end
 
-    new_root = node_insert(new_root, size - @width, tail, new_shift)
-    tail = node_new(value)
+    new_root = Node.insert(new_root, size - @width, tail, new_shift)
+    tail = Node.new(value)
 
     %__MODULE__{vector |
       root: new_root,
@@ -71,62 +122,19 @@ defmodule PersistentVector do
       size: size + 1}
   end
 
-  def node_insert(nil, key, node, level) when level > 0 do
-    idx = band(key >>> level, @mask)
-    tree = node_new(nil)
-    child = node_insert(elem(tree, idx), key, node, level - @bits)
-    put_elem(tree, idx, child)
-  end
-  def node_insert(nil, _, node, _), do: node
-  def node_insert(tree, key, node, level) when level > 0 do
-    idx = band(key >>> level, @mask)
-    child = node_insert(elem(tree, idx), key, node, level - @bits)
-    put_elem(tree, idx, child)
-  end
-  def node_insert(tree, key, node, _) do
-    idx = band(key >>> @bits, @mask)
-    put_elem(tree, idx, node)
-  end
-
   def set(vector = %__MODULE__{tail: tail, tail_offset: offset}, key, value) when key >= offset do
     %__MODULE__{vector | tail: put_elem(tail, key - offset, value)}
   end
   def set(vector = %__MODULE__{root: root, shift: shift}, key, value) do
-    %__MODULE__{vector | root: node_set(root, key, value, shift)}
-  end
-
-  def node_set(nil, _, value, level) do
-    Enum.reduce(0..div(level, @bits), value, fn _, acc ->
-      node_new(acc)
-    end)
-  end
-  def node_set(node, key, value, level) when level > 0 do
-    idx = band(key >>> level, @mask)
-    child = elem(node, idx)
-    put_elem(node, idx, node_set(child, key, value, level - @bits))
-  end
-  def node_set(node, key, value, _) do
-    idx = band(key, @mask)
-    put_elem(node, idx, value)
+    %__MODULE__{vector | root: Node.set(root, key, value, shift)}
   end
 
   def from_list(list), do: Enum.reduce(list, new, &(append(&2, &1)))
 
   def to_list(%__MODULE__{root: root, size: size, shift: shift, tail: tail, tail_offset: offset}) do
-    head = node_to_list(root, shift) |> Enum.take(offset)
+    head = Node.to_list(root, shift) |> Enum.take(offset)
     tail = Tuple.to_list(tail) |> Enum.take(size - offset)
     head ++ tail
-  end
-
-  def node_to_list(nil, _), do: []
-  def node_to_list(node, 0), do: Tuple.to_list(node)
-  def node_to_list(node, level) do
-    Tuple.to_list(node)
-    |> Enum.flat_map(&node_to_list(&1, level - @bits))
-  end
-
-  def node_new(value) do
-    Tuple.duplicate(nil, @width) |> put_elem(0, value)
   end
 end
 
